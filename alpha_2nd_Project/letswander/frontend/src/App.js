4824e2c293d76cc50ff229296bce5448f1e87f65
alapan-03 <kalx.alapankar.1748395@gmail.com>
2024-12-08 11:19:09 +0530

    fix/websocket url in frontend

--- alpha_2nd_Project/letswander/frontend/src/App.js
+++ alpha_2nd_Project/letswander/frontend/src/App.js
@@ -26,7 +26,7 @@ import CustomToast from './Components/CustomToast';
 
 export const UserContext = createContext()
 
-const socket = io('http://localhost:3000');
+const socket = io('https://letswander-full-new.onrender.com');
 
 
 
